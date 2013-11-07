583bb290914e9433890793b9725280273fde2c60
Jonathan Underwood <jonathan.underwood@gmail.com>
2013-11-07 16:03:06 +0000

    Properly parametrize the main class Remove apache configuration from this module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,12 @@
-class mirrorserver {
-
-  include mirrorserver::params
-  include apache
+class mirrorserver (
+  $www_root = $mirrorserver::params::www_root,
+  $rsync_opts = $mirrorserver::params::rsync_opts,
+  $mirror_script = $mirrorserver::params::mirror_script,
+  $reposync_conf = $mirrorserver::params::reposync_conf,
+  $lockfile = $mirrorserver::params::lockfile,
+)
+{
   include concat::setup
-  
-  $www_root = $mirrorserver::params::www_root
   
   file {$www_root:
     ensure => directory,
@@ -12,10 +14,7 @@ class mirrorserver {
     group  => root,
     mode   => 0755,
   }
-  
-  $script = $mirrorserver::params::mirror_script
-  $lockfile = $mirrorserver::params::lockfile
-  
+
   concat {$script:
     owner  => root,
     group  => root,
@@ -34,8 +33,6 @@ class mirrorserver {
     content => template('mirrorserver/script_footer.erb'),
   }
 
-  $reposync_conf = $mirrorserver::params::reposync_conf
-
   concat {$reposync_conf:
     owner  => root,
     group  => root,
@@ -46,12 +43,5 @@ class mirrorserver {
     target  => $reposync_conf,
     order   => 000,
     content => template('mirrorserver/reposync_header.erb'),
-  }
-  
-  apache::vhost {'mirrors':
-    priority => 10,
-    port     => 80,
-    docroot  => $www_root,
-    require  => File [$www_root],
   }
 }
